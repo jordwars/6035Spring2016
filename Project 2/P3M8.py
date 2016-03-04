55fd9ab6bd6f1a9b46710e71bca1f1a9a2a992d5
Jordan <jordwars@gmail.com>
2016-03-04 01:28:59 -0500

    Proper conversion of int to str

--- Project 2/P3M8.py
+++ Project 2/P3M8.py
@@ -5,6 +5,6 @@ day = 1
 os.system("cd Desktop/cuckoo") #placing the terminal in the right place
 #need to call every day in march at 2pm
 while day <= 31:
-    os.system('python ./utils/submit.py ­­platform windows ­­machine WindowsXPSP3 ­­timeout 180 --clock "03-'+string(day)+'-2016 14:00:00" ../malware/PhaseIII/malware8.exe'')
+    os.system('python ./utils/submit.py ­­platform windows ­­machine WindowsXPSP3 ­­timeout 180 --clock "03-'+str(day)+'-2016 14:00:00" ../malware/PhaseIII/malware8.exe'')
     day = day +1 
 #create loop that runs through every day
