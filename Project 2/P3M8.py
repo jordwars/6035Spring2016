aea66dd042e4c38b03dd7f314599b02f84b4b278
Jordan <jordwars@gmail.com>
2016-03-04 01:25:51 -0500

    Configured faulty Eclipse and first draft of script
    eclipse parts are worthless but hopefully the script works, if it
    doesn't I need to format the day properly

---
+++ Project 2/P3M8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+# need to use os.system to issue commands, takes in as string
+day = 1
+os.system("cd Desktop/cuckoo") #placing the terminal in the right place
+#need to call every day in march at 2pm
+while day <= 31:
+    os.system('python ./utils/submit.py ­­platform windows ­­machine WindowsXPSP3 ­­timeout 180 --clock "03-'+string(day)+'-2016 14:00:00" ../malware/PhaseIII/malware8.exe'')
+    day = day +1 
+#create loop that runs through every day
